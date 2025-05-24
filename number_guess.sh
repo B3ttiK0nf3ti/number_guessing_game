0b68072f43a0b3e7825a72610c19b763c7590ab9
Bettina Weigandt <weigandt.bettina@gmail.com>
2025-05-24 07:43:47 +0000

    feat: check if integer

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,37 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$((1 + $RANDOM % 1000))
+GUESS_COUNT=0
 
+# starting screen
 echo "Enter your username:"
 read USERNAME
+
+# Check if user exists
+USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME';")
+
+if [[ -z $USER_DATA ]]; then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # Insert new user
+  INSERT_RESULT=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, NULL);")
+else
+  # Existing user - parse data
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+echo "Guess the secret number between 1 and 1000:"
+
+while true;
+do
+  read USER_GUESS
+  if ! [[ "$USER_GUESS" =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((GUESS_COUNT++))
+
+  
+
